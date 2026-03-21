b9e08fa2c752142ddc2bfffe784f73accdc8d926
Luigi <luigitruglioac@gmail.com>
2026-03-21 20:41:33 -0300

    adicionada exception ConsoleNaoExisteException

---
+++ src/main/java/CatalogoDeJogos/ConsoleNaoExisteException.java
@@ -0,0 +1,7 @@
+package CatalogoDeJogos;
+
+public class ConsoleNaoExisteException extends RuntimeException {
+    public ConsoleNaoExisteException(String message) {
+        super(message);
+    }
+}
